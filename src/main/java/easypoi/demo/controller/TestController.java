71481ccfa4da3f3d9adf6f09d4114f88d450d3c5
IanHJW <Ianfengh@163.com>
2018-04-23 10:19:33 +0800

    修改新增测试文件test

--- src/main/java/easypoi/demo/controller/TestController.java
+++ src/main/java/easypoi/demo/controller/TestController.java
@@ -1,12 +1,12 @@
 /**
  * Author: hjw
- * Data: 2018/4/23 10:13
- * Description: 测试github上新增文件
+ * Data: 2018/4/23 10:17
+ * Description: 测试GitHub上新增文件
  */
 package easypoi.demo.controller;
 
 public class TestController {
-    public void test(){
-        System.out.println("这是新增文件！");
+    public void t(){
+
     }
 }
